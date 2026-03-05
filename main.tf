ac56db2916dd5b0f8b95de104bac9d8bc37ceb20
jalal abdul <jalal.abdulnasir@gmail.com>
2026-03-06 01:09:07 +0400

    final lab v3

--- main.tf
+++ main.tf
@@ -33,21 +33,21 @@ data "aws_ami" "ubuntu" {
 resource "aws_vpc" "VPC1" {
 
 Name = "Testing"
-cidr_block = ["10.0.0.0/16"]
+cidr_block = "10.0.0.0/16"
 
 }
 
 resource "aws_subnet" "Private" {
 
 vpc_id = aws_vpc.VPC1.id
-cidr_block = ["10.0.1.0/24"]
+cidr_block = "10.0.1.0/24"
 
 }
 
 resource "aws_subnet" "Public" {
 
 vpc_id = aws_vpc.VPC1.id
-cidr_block = ["10.0.2.0/24"]
+cidr_block = "10.0.2.0/24"
 map_public_ip_on_launch = true
 
 }
