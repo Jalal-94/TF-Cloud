4b76435f4d184599064cf3b601956ee9c4b39d8a
jalal abdul <jalal.abdulnasir@gmail.com>
2026-03-06 14:52:59 +0400

    added variables

--- main.tf
+++ main.tf
@@ -32,8 +32,12 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_vpc" "VPC1" {
 
-Name = "Testing"
 cidr_block = "10.0.0.0/16"
+
+tags = {
+
+  Name = "VPC1"
+  created_time = formatdate("YYYY-MM-DD hh:mm ZZZ",timestamp())
 
 }
 
