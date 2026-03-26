20129de07b16aac22bf06f42a73e91decc0cba26
Jalal-94 <jalal.abdunasir@gmail.com>
2026-03-26 22:33:53 +0400

    SSH port updated

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ vpc_id = aws_vpc.VPC1.id
 
 ingress {
 
-from_port = 0
+from_port = 22
 to_port = 22
 protocol = "tcp"
 cidr_blocks = ["0.0.0.0/0"]
@@ -133,7 +133,7 @@ cidr_blocks = ["0.0.0.0/0"]
 egress {
 
 from_port =  22
-to_port = 0
+to_port = 22
 protocol = "tcp"
 cidr_blocks = ["0.0.0.0/0"]
 
