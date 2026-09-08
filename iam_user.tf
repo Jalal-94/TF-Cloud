f5c07513665c0633419e5c776d249990935e51d0
jalal abdul <jalal.abdulnasir@gmail.com>
2026-09-08 15:23:58 +0400

    count meta-arg

--- iam_user.tf
+++ iam_user.tf
@@ -1,62 +1,26 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "6.61.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.26"
     }
-    
   }
 
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.14.2"
 }
 
 provider "aws" {
-  
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
-data "aws_ami" "amazon_linux" {
-  
- most_recent = true
- owners = ["amazon"]
-
- filter {
-   name = "name"
-   values = ["al2023-ami-*-x86_64"]
- }
-
- filter {
-
-name = "virtualization-type"
-values = ["hvm"]
-
- }
-  
-}
-
-
-
-resource "aws_instance" "LAB1" {
-
-  ami = data.aws_ami.amazon_linux.id
-  count = 3
-  instance_type = var.instance_type
-
+resource "aws_instance" "count_lab" {
+  # add your count meta-argument here!
+  count = 2
+  ami           = "ami-050352a65e954abb1"
+  instance_type = "t2.micro"
+    
   tags = {
-    name = "instance-${count.index}"
+    # Modify the Name tag so that it uses the count_index argument!
+    Name = "Count-Lab-${count.index}"
   }
-
-lifecycle {
-  precondition {
-    condition = !contains(["t2.micro","t2.micro"],var.instance_type)
-    error_message = "instance type not matches the allowed t2.micro or t3.micro got ${var.instance_type} instead"
-  }
-}
-
-  
-}
-
-variable "instance_type" {
-
- default = "t1.micro" 
 }
